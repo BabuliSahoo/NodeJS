5c13f44f36247e1afe8e8da22e04002cdb1003f8
Babuli Sahoo <babuli.sahoo4@gmail.com>
2024-06-12 01:37:17 +0530

    Route and finishing

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
-//const http = require("http");
-//const routes = require('./routes');
-const express = require("express");
-const bodyParser = require("body-parser");
-const path =  require('path');
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/admin',adminRoutes);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-//Handle Error with Status
 app.use((req, res, next) => {
-  //res.status(404).send("<h1>Page not ound</h1>");
-  res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-
-//console.log(routes.someText);
-//const server = http.createServer(routes.handler);
-// const server = http.createServer(app);
-// server.listen(3003);
 
 app.listen(3000);
